4ec837e38c0c2a4238f63482a0f866e589ebbfc1
zhuyuying <24320162202971@stu.xmu.edu.cn>
2022-05-27 21:07:59 +0800

    init

--- train.py
+++ train.py
@@ -2,7 +2,6 @@ import time
 from options.train_options import TrainOptions
 from data import create_dataset
 from models import create_model
-from util.visualizer import Visualizer
 
 if __name__ == '__main__':
     opt = TrainOptions().parse()   # get training options
